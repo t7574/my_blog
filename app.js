788a54025814e346f28227b1a1df743b48d82536
poke <taihai@qq.com>
2018-12-16 00:05:39 +0800

    //

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 
 const app = express()
-const moment=require("moment")
+const moment = require("moment")
 const conn = require('./db/index.js')
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({
@@ -31,11 +31,11 @@ app.post("/register", (req, res) => {
     const body = req.body
     if (body.username.trim().length <= 0 || body.password.trim().length <= 0 ||
         body.nickname.trim().length <= 0) {
-            res.send({
-                status: 501,
-                msg: "请填写正确的表单格式数据在进行注册"
-            })
-        }
+        res.send({
+            status: 501,
+            msg: "请填写正确的表单格式数据在进行注册"
+        })
+    }
     const sqlregister = "select count(*) as count from my_blog where username=? "
     conn.query(sqlregister, body.username, (err, result) => {
         if (err) return res.send({
@@ -43,28 +43,43 @@ app.post("/register", (req, res) => {
             msg: '用户名查重失败'
         })
     })
-    body.ctime=moment().format("YYYY-MM-DD HH:mm:ss")
-    const sql1="insert into my_blog set ?" 
-    conn.query(sql1,body,(err,result) => {
-   if(err) return res.send({status:400,msg:"注册失败"})
-   if(result.affectedRows!==1) return res.send({status:401,msg:"注册失败"})
-   res.send({
-    status: 200,
-    msg: "注册成功"
-})
+    body.ctime = moment().format("YYYY-MM-DD HH:mm:ss")
+    const sql1 = "insert into my_blog set ?"
+    conn.query(sql1, body, (err, result) => {
+        if (err) return res.send({
+            status: 400,
+            msg: "注册失败"
+        })
+        if (result.affectedRows !== 1) return res.send({
+            status: 401,
+            msg: "注册失败"
+        })
+        res.send({
+            status: 200,
+            msg: "注册成功"
+        })
     })
-  
+
 })
 // 登录页面监听
-app.post("/login",(req,res) =>{
-const body=req.body
-// console.log(body)
-const sqlLogin="select * from my_blog where username=? and password=?"
-conn.query(sqlLogin,[body.username,body.password], (err,result) =>{
-    if(err) return res.send({status:500,msg:"用户名或密码错误"})
-    if (result.length !== 1) return res.send({ msg: '用户登录失败', status: 502 })
-    res.send({status:200,msg:"登录成功"})
-})
+app.post("/login", (req, res) => {
+    const body = req.body
+    // console.log(body)
+    const sqlLogin = "select * from my_blog where username=? and password=?"
+    conn.query(sqlLogin, [body.username, body.password], (err, result) => {
+        if (err) return res.send({
+            status: 500,
+            msg: "用户名或密码错误"
+        })
+        if (result.length !== 1) return res.send({
+            msg: '用户登录失败',
+            status: 502
+        })
+        res.send({
+            status: 200,
+            msg: "登录成功"
+        })
+    })
 })
 app.listen(3000, () => {
 
